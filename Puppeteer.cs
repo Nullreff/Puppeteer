5a211da7ec3138f1242ef4740bc3a8c1ed0c466b
Nullreff <git@nullreff.net>
2023-04-13 04:48:38 -0700

    Initial commit

---
+++ Puppeteer.cs
@@ -0,0 +1,29 @@
+﻿using NeosModLoader;
+using HarmonyLib;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SkinnedMeshRenderer = FrooxEngine.SkinnedMeshRenderer;
+using BaseX;
+using FrooxEngine.CommonAvatar;
+using FrooxEngine.LogiX.WorldModel;
+using System.Reflection;
+
+namespace Puppeteer
+{
+    public class Puppeteer : NeosMod
+    {
+        public override string Name => "Puppeteer";
+        public override string Author => "nullreff";
+        public override string Version => "0.0.1";
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.nullreff.puppeteer");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Puppeteer
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows for creating clones that can be puppeted and controlled by the original.
+
+## Installation
+
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [Puppeteer.dll]() into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+
+## Usage
+1. TODO
